f491e7c1db79950298c3b90f72bbb82425137480
MOZGIII <mike-n@narod.ru>
2020-04-10 21:37:57 +0300

    Add counters to server

--- server/main.go
+++ server/main.go
@@ -6,16 +6,37 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync/atomic"
 	"time"
 )
 
+var totalBytesRead uint64
+var totalRequests uint64
+
 func handle(w http.ResponseWriter, req *http.Request) {
-	_, err := io.Copy(ioutil.Discard, req.Body)
+	bytesRead, err := io.Copy(ioutil.Discard, req.Body)
 	if err != nil {
 		panic(err)
 	}
+	atomic.AddUint64(&totalRequests, 1)
+	atomic.AddUint64(&totalBytesRead, uint64(bytesRead))
 	time.Sleep(100 * time.Millisecond)
 	w.WriteHeader(200)
+}
+
+func printStats() {
+	for {
+		bytesSecondAgo := atomic.LoadUint64(&totalBytesRead)
+		requestsSecondAgo := atomic.LoadUint64(&totalRequests)
+		time.Sleep(3 * time.Second)
+		bytesNow := atomic.LoadUint64(&totalBytesRead)
+		requestsNow := atomic.LoadUint64(&totalRequests)
+		fmt.Printf(
+			"B: %016d (%016d) R: %08d (%08d)\n",
+			bytesNow-bytesSecondAgo, bytesNow,
+			requestsNow-requestsSecondAgo, requestsNow,
+		)
+	}
 }
 
 func main() {
@@ -24,6 +45,7 @@ func main() {
 		addr = ":9001"
 	}
 	fmt.Printf("Listening at %s\n", addr)
+	go printStats()
 	if err := http.ListenAndServe(addr, http.HandlerFunc(handle)); err != nil {
 		panic(err)
 	}
